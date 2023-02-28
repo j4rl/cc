f59aa626cf8e1d79793eb54ba917805c8957c9d7
j4rl <charlie.jarl@gmail.com>
2023-02-28 09:58:31 +0100

    stuff

--- app.js
+++ app.js
@@ -25,4 +25,55 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
     createBoard();
+
+    //Dragging stuff around... and... stuff.
+    let colorBeingDragged;
+    let colorBeingReplaced;
+    let squareIdBeingDragged;
+    let squareIdBeingReplaced;
+
+    squares.forEach(square => square.addEventListener('dragstart', dragStart));
+    squares.forEach(square => square.addEventListener('dragend', dragEnd));
+    squares.forEach(square => square.addEventListener('dragover',dragOver));
+    squares.forEach(square => square.addEventListener('dragenter',dragEnter));
+    squares.forEach(square => square.addEventListener('dragleave',dragLeave));
+    squares.forEach(square => square.addEventListener('drop',dragDrop));
+
+    function dragStart(){
+        colorBeingDragged=this.style.background;
+        squareIdBeingDragged=parseInt(this.id);
+    }
+    function dragEnd(){
+        let validMoves=[
+            squareIdBeingDragged-1,
+            squareIdBeingDragged-width,
+            squareIdBeingDragged+1,
+            squareIdBeingDragged+width
+        ];
+        let validMove=validMoves.includes(squareIdBeingReplaced);
+        if(squareIdBeingReplaced && validMove){
+            squareIdBeingReplaced=null;
+        }else if(squareIdBeingReplaced && !validMove){
+            squares[squareIdBeingReplaced].style.background=colorBeingReplaced;
+            squares[squareIdBeingDragged].style.background=colorBeingDragged;
+        }else{
+            squares[squareIdBeingDragged].style.background=colorBeingDragged;
+        }
+
+    }
+    function dragOver(){
+        e.preventDefault();   
+    }
+    function dragEnter(){
+        e.preventDefault();
+    }
+    function dragLeave(){
+
+    }
+    function dragDrop(){
+        colorBeingReplaced=this.style.background;
+        squareIdBeingReplaced=parseInt(this.id);
+        this.style.background=colorBeingDragged;
+        squares[squareIdBeingDragged].style.background=colorBeingReplaced;
+    }
 })
