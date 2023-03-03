06612e327905d77cf381c9683167970232d2ff62
j4rl <charlie.jarl@gmail.com>
2023-03-03 11:27:24 +0100

    latest

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ document.addEventListener("DOMContentLoaded", function() {
             square.style.background=candyColors[randomColor];
             square.setAttribute('id', i);
             square.setAttribute('draggable', true);
+
+            square.innerHTML=i;  //Temporär
+            square.classList.add("cen"); //Temporär
+
             grid.appendChild(square);
             squares.push(square);
         }
@@ -79,7 +83,55 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function checkMove(checkCase, points){
-        
+        for(i=0;i<width*width;i++){
+            switch(checkCase){
+                case "threerow":
+                    arrCase=[i, i+1, i+2];
+                    arrNoGo=[6,7,14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+                    break;
+                case "threecolumn":
+                    arrCase=[i, i+width, i+width*2];
+                    arrNoGo=[48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63];
+                    break;
+                default:
+            }
+
+            let decidedColor=squares[i].style.background;
+            const isBlank=squares[i].style.background === "";
+
+            if(arrNoGo.includes(i)) continue; 
+
+            if(arrCase.every(index => squares[index].style.background === decidedColor && !isBlank)){
+                score += points;
+                document.getElementById('score').innerHTML=score;
+                arrCase.forEach(index => {
+                    squares[index].style.background = "";
+                })
+            }
+        }
     }
+
+    function moveDown(){
+        for(i=0;i<width*width;i++){
+            if(squares[i].style.background === ""){
+                squares[i].style.background = squares[i - width].style.background;
+                squares[i - width].style.background="";
+            }
+        }
+        for(i=0;i<width;i++){
+            if(squares[i].style.background === ""){
+                let randomColor=Math.floor(Math.random*candyColors.length);
+                squares[i].style.background=candyColors[randomColor];
+            }
+        }
+    }
+
+window.setInterval(function(){
+    checkMove("threerow", 10);
+    checkMove("threecolumn", 10);
+    moveDown();
+}, 100);
+
+
     
 })
