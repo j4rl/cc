5a17493fc0d40de03f588cff828f1ccc228f4bb2
j4rl <charlie.jarl@gmail.com>
2023-03-05 19:37:51 +0100

    Still an error

--- app.js
+++ app.js
@@ -93,8 +93,60 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrCase=[i, i+width, i+width*2];
                     arrNoGo=[48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63];
                     break;
+                /*case "fourrow":
+                    arrCase=[i, i+1, i+2, i+3];
+                    arrNoGo=[5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,61,62,63];
+                    break;
+                case "fourcolumn":
+                    arrCase=[i, i+width, i+width*2, i+width*3];
+                    arrNoGo=[40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63];
+                    break;
+                case "fiverow":
+                    arrCase=[i, i+1, i+2, i+3, i+4];
+                    arrNoGo=[4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31,36,37,38,39,44,45,46,47,52,53,54,55,60,61,62,63];
+                    break;
+                case "fivecolumn":
+                    arrCase=[i, i+width, i+width*2, i+width*3, i+width*4];
+                    arrNoGo=[32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63];
+                    break;
+                case "smallpool":
+                    //  X X
+                    //  X X
+                    arrCase=[i, i+1, i+width, i+width+1];
+                    arrNoGo=[7, 15, 23, 31, 39, 47, 55,56,57,58,59,60,61,62 63];
+                    break;
+                case "bigpool": 
+                    //  X X X
+                    //  X X X
+                    //  X X X 
+                    arrCase=[i, i+1, i+2, i+width, i+width+1, i+width+2, i+width*2, i+width*2+1, i+width*2+2];
+                    arrNoGo=[6,7,14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 48,49,50,51,52,53,54,55,56,57,58,59,60,61, 62, 63];
+                    break;
+                case "diamond":
+                    //    X
+                    //  X   X
+                    //    X
+                    break;
+                case "cross":
+                    //    X
+                    //  X X X
+                    //    X
+                    break;
+                case "fourleftdiagonal":
+                    //  X
+                    //    X
+                    //      X
+                    //        X
+                    break;
+                case "fourrightdiagonal":
+                    //        X
+                    //      X
+                    //    X
+                    //  X
+                    break;    */
                 default:
             }
+
 
             let decidedColor=squares[i].style.background;
             const isBlank=squares[i].style.background === "";
@@ -112,27 +164,25 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function moveDown(){
-        for(i=0;i<width*width;i++){
-            if(squares[i].style.background === ""){
-                squares[i].style.background = squares[i - width].style.background;
-                squares[i - width].style.background="";
-            }
-        }
         for(i=0;i<width;i++){
             if(squares[i].style.background === ""){
                 let randomColor=Math.floor(Math.random * candyColors.length);
                 squares[i].style.background=candyColors[randomColor];
             }
         }
+        for(i=0;i<width*width;i++){
+            if(squares[i].style.background === ""){
+                squares[i].style.background = squares[i - width].style.background;
+                squares[i - width].style.background = "";
+            }
+        }
+
     }
 
-window.setInterval(function(){
-    moveDown();
-    checkMove("threerow", 10);
-    checkMove("threecolumn", 10);
-    
-}, 100);
-
-
-    
+    window.setInterval(function() {
+        checkMove("threerow", 10);
+        checkMove("threecolumn", 10);
+        moveDown();    
+    }, 100);
+   
 })
