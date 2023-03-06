7dcf874960c7581926443249182277b07f2b53ef
j4rl <charlie.jarl@gmail.com>
2023-03-06 08:45:45 +0100

    Not final

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ document.addEventListener("DOMContentLoaded", function() {
             square.setAttribute('id', i);
             square.setAttribute('draggable', true);
 
-            square.innerHTML=i;  //Temporär
-            square.classList.add("cen"); //Temporär
+            //square.innerHTML=i;  //Temporär
+            //square.classList.add("cen"); //Temporär
 
             grid.appendChild(square);
             squares.push(square);
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrCase=[i, i+width, i+width*2];
                     arrNoGo=[48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63];
                     break;
-                /*case "fourrow":
+                case "fourrow":
                     arrCase=[i, i+1, i+2, i+3];
                     arrNoGo=[5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,61,62,63];
                     break;
@@ -113,7 +113,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     //  X X
                     //  X X
                     arrCase=[i, i+1, i+width, i+width+1];
-                    arrNoGo=[7, 15, 23, 31, 39, 47, 55,56,57,58,59,60,61,62 63];
+                    arrNoGo=[7, 15, 23, 31, 39, 47, 55,56,57,58,59,60,61,62, 63];
                     break;
                 case "bigpool": 
                     //  X X X
@@ -143,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     //      X
                     //    X
                     //  X
-                    break;    */
+                    break;    
                 default:
             }
 
@@ -166,7 +166,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function moveDown(){
         for(i=0;i<width;i++){
             if(squares[i].style.background === ""){
-                let randomColor=Math.floor(Math.random * candyColors.length);
+                let randomColor=Math.floor(Math.random() * candyColors.length);
                 squares[i].style.background=candyColors[randomColor];
             }
         }
